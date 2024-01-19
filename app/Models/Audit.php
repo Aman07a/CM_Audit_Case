08fb21849378ccc5fff531cd7857b1382076b80f
Aman07a <aman16.aa17@gmail.com>
2024-01-19 22:05:27 +0100

    Import All Classes

--- app/Models/Audit.php
+++ app/Models/Audit.php
@@ -2,8 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+use App\Models\Answer;
+use App\Models\Customer;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Audit extends Model
 {
